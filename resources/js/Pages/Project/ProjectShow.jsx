cef478f66483607f51845ea5438d14b622fd8e19
Alin Chindea <alin.chindea@gmail.com>
2024-05-20 22:16:38 +0200

    Cleanup

--- resources/js/Pages/Project/ProjectShow.jsx
+++ resources/js/Pages/Project/ProjectShow.jsx
@@ -21,7 +21,6 @@ export default function ProjectShow({ auth, project, tasks, queryParams }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <p className="text-black">Tasks {tasks.length}</p>
                         <div>
                             <img
                                 src={project.image_path}
