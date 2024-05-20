4c7e702003b8bec00f80680abab671007db14a3e
Alin Chindea <alin.chindea@gmail.com>
2024-05-20 17:55:15 +0200

    Add option to create a project
    - markdown plus create method in controller
    - logic to route to the store method on submit

---
+++ resources/js/Pages/Project/ProjectCreate.jsx
@@ -0,0 +1,165 @@
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
+import TextareaInput from "@/Components/TextareaInput";
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+import { Head, Link, useForm } from "@inertiajs/react";
+
+export default function ProjectCreate({ auth }) {
+    const { data, setData, post, errors, reset } = useForm({
+        image: "",
+        name: "",
+        status: "",
+        description: "",
+        deadline: "",
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        post(route("projects.store"));
+    };
+
+    return (
+        <Authenticated
+            user={auth.user}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Add new project
+                </h2>
+            }
+        >
+            <Head title="Create project" />
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <form
+                            onSubmit={handleSubmit}
+                            className="p-4 sm:p-8 bg-white shadow sm:rounded-lg"
+                        >
+                            <div>
+                                <InputLabel
+                                    htmlFor="project_image_path"
+                                    value="Project Image"
+                                />
+                                <TextInput
+                                    id="project_image_path"
+                                    type="file"
+                                    name="image"
+                                    value={data.image}
+                                    className="mt-1 block w-full"
+                                    onChange={(e) =>
+                                        setData("image", e.target.value)
+                                    }
+                                />
+                                <InputError
+                                    message={errors.image}
+                                    className="mt-2"
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <InputLabel
+                                    htmlFor="project_name"
+                                    value="Project Name"
+                                />
+                                <TextInput
+                                    id="project_name"
+                                    type="text"
+                                    name="name"
+                                    value={data.name}
+                                    className="mt-1 block w-full"
+                                    isFocused={true}
+                                    onChange={(e) =>
+                                        setData("name", e.target.value)
+                                    }
+                                />
+                                <InputError
+                                    message={errors.name}
+                                    className="mt-2"
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <InputLabel
+                                    htmlFor="project_description"
+                                    value="Project Description"
+                                />
+                                <TextareaInput
+                                    id="project_description"
+                                    name="description"
+                                    value={data.description}
+                                    className="mt-1 block w-full"
+                                    onChange={(e) =>
+                                        setData("description", e.target.value)
+                                    }
+                                />
+                                <InputError
+                                    message={errors.description}
+                                    className="mt-2"
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <InputLabel
+                                    htmlFor="project_deadline"
+                                    value="Project Deadline"
+                                />
+                                <TextInput
+                                    id="project_deadline"
+                                    type="date"
+                                    name="deadline"
+                                    value={data.deadline}
+                                    className="mt-1 block w-full"
+                                    onChange={(e) =>
+                                        setData("deadline", e.target.value)
+                                    }
+                                />
+                                <InputError
+                                    message={errors.deadline}
+                                    className="mt-2"
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <InputLabel
+                                    htmlFor="project_status"
+                                    value="Project Status"
+                                />
+
+                                <SelectInput
+                                    name="status"
+                                    id="project_status"
+                                    className="mt-1 block w-full"
+                                    onChange={(e) =>
+                                        setData("status", e.target.value)
+                                    }
+                                >
+                                    <option value="">Select Status</option>
+                                    <option value="pending">Pending</option>
+                                    <option value="in_progress">
+                                        In Progress
+                                    </option>
+                                    <option value="completed">Completed</option>
+                                </SelectInput>
+
+                                <InputError
+                                    message={errors.project_status}
+                                    className="mt-2"
+                                />
+                            </div>
+                            <div className="mt-4 text-right">
+                                <Link
+                                    href={route("project.index")}
+                                    className="bg-gray-100 py-1 px-3 text-gray-800 rounded shadow transition-all hover:bg-gray-200 mr-2"
+                                >
+                                    Cancel
+                                </Link>
+                                <button className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600">
+                                    Submit
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </Authenticated>
+    );
+}
--- resources/js/Pages/Project/ProjectIndex.jsx
+++ resources/js/Pages/Project/ProjectIndex.jsx
@@ -46,9 +46,16 @@ export default function ProjectIndex({ auth, projects, queryParams = null }) {
         <AuthenticatedLayout
             user={auth.user}
             header={
-                <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                    Projects
-                </h2>
+                <div className="flex items-center justify-between">
+                    <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                        Projects
+                    </h2>
+                    <Link
+                        href={route("project.create")}
+                        className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all">
+                        Create Project
+                    </Link>
+                </div>
             }
         >
             <Head title="Projects" />
