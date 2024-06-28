08c207d86a1a410601d997eb8a851eb3198c15d3
Alin Chindea <alin.chindea@gmail.com>
2024-06-28 14:39:46 +0200

    Add an overview of active tasks on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -9,12 +10,20 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        $user_id = auth()->user()->id;
         $totalPendingTasks = Task::where('status', 'open')->count();
-        $usersPendingTasks = Task::where('status', 'open')->where('assigned_to', auth()->user()->id)->count();
+        $usersPendingTasks = Task::where('status', 'open')->where('assigned_to', $user_id)->count();
         $totalOngoingTasks = Task::where('status', 'in_progress')->count();
-        $usersOngoingTasks = Task::where('status', 'in_progress')->where('assigned_to', auth()->user()->id)->count();
+        $usersOngoingTasks = Task::where('status', 'in_progress')->where('assigned_to', $user_id)->count();
         $totalCompletedTasks = Task::where('status', 'completed')->count();
-        $usersCompletedTasks = Task::where('status', 'completed')->where('assigned_to', auth()->user()->id)->count();
+        $usersCompletedTasks = Task::where('status', 'completed')->where('assigned_to', $user_id)->count();
+        $activeTasks = TaskResource::collection(
+            Task::whereIn('status', ['open', 'in_progress'])
+                ->where('assigned_to', $user_id)
+                ->limit(10)
+                ->get()
+        );
+
         return inertia('Dashboard', [
             'totalPendingTasks' => $totalPendingTasks,
             'usersPendingTasks' => $usersPendingTasks,
@@ -22,6 +31,7 @@ class DashboardController extends Controller
             'usersOngoingTasks' => $usersOngoingTasks,
             'totalCompletedTasks' => $totalCompletedTasks,
             'usersCompletedTasks' => $usersCompletedTasks,
+            'activeTasks' => $activeTasks,
         ]);
     }
 }
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import TaskStatusReportCard from '@/Components/TaskStatusReportCard';
-import { Head } from '@inertiajs/react';
+import TableHeading from "@/Components/TableHeading";
+import { Head, Link } from '@inertiajs/react';
+import { TASK_STATUS_CLASS_MAP, TASK_STATUS_TEXT_MAP } from "@/constants.jsx";
 
 export default function Dashboard({
     auth,
@@ -9,7 +11,9 @@ export default function Dashboard({
     usersOngoingTasks,
     totalOngoingTasks,
     usersCompletedTasks,
-    totalCompletedTasks }) {
+    totalCompletedTasks,
+    activeTasks
+}) {
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -22,6 +26,61 @@ export default function Dashboard({
                     <TaskStatusReportCard className='text-amber-500' user={auth.user} title="Pending tasks" usersStats={usersPendingTasks} totalStats={totalPendingTasks} />
                     <TaskStatusReportCard className='text-blue-500' user={auth.user} title="In progress tasks" usersStats={usersOngoingTasks} totalStats={totalOngoingTasks} />
                     <TaskStatusReportCard className='text-green-500' user={auth.user} title="Completed tasks" usersStats={usersCompletedTasks} totalStats={totalCompletedTasks} />
+                </div>
+
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
+                    <div className='bg-white overflow-hidden shadow-sm sm:rounded-lg'>
+                        <div className='p-6 text-gray-900 dark:text-gray-100'>
+                            <h3 className="text-gray-900 text-xl font-semibold mb-4">
+                                Overview of active tasks
+                            </h3>
+
+                            <table>
+                                <thead className="text-xs text-gray-900 uppercase bg-gray-300">
+                                    <tr>
+                                        <TableHeading name="id" sortable={false}>ID</TableHeading>
+                                        <TableHeading name="project_tile" sortable={false}>Project Title</TableHeading>
+                                        <TableHeading name="name" sortable={false}>Name</TableHeading>
+                                        <TableHeading name="status" sortable={false}>Status</TableHeading>
+                                        <TableHeading name="deadline" sortable={false}>Due Date</TableHeading>
+                                    </tr>
+                                </thead>
+                                <tbody className="divide-y divide-gray-200">
+                                    {activeTasks.data.map((task) => (
+                                        <tr key={task.id}>
+                                            <td className="whitespace-nowrap px-4 py-3 font-medium text-gray-900">
+                                                {task.id}
+                                            </td>
+                                            <td className="whitespace-nowrap px-4 py-3 text-gray-700 hover:underline cursor-pointer">
+                                                <Link href={route('project.show', task.project.id)}>
+                                                    {task.project.name}
+                                                </Link>
+                                            </td>
+                                            <td className="px-4 py-3 text-gray-700 hover:underline cursor-pointer">
+                                                <Link href={route('task.show', task.id)}>
+                                                    {task.name}
+                                                </Link>
+                                            </td>
+                                            <td className="whitespace-nowrap px-4 py-3 text-gray-700">
+                                    <span
+                                        className={`px-2 py-1 rounded text-white ${
+                                            TASK_STATUS_CLASS_MAP[task.status]
+                                        }`}
+                                    >
+                                        {TASK_STATUS_TEXT_MAP[task.status]}
+                                    </span>
+                                </td>
+                                            <td className="whitespace-nowrap px-4 py-3 text-gray-700">
+                                    {task.due_date}
+                                </td>
+                                        </tr>
+
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </AuthenticatedLayout>
